816130776b10467f0036935979b3ae965649c80d
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 22:21:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,8 +4,15 @@ for (let i=0; i<25; i++) {
   arr.push(`I am 1 strange loop.`);
   }
   else {
-    arr.push(`I am ${i} strange loops.`)
+    arr.push(`I am ${i} strange loops.`);
     }
   }
   return arr;
+}
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(num);
+    num--;
+  }
 }
