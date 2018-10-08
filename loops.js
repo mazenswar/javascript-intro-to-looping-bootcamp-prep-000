40df6824b510e7fbf19c07f94b49642840535cc5
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 22:17:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
 function forLoop(arr) {
 for (let i=0; i<25; i++) {
-  if (i === 0) {}
-  else if(i === 1) {
+  if(arr[i] === 1) {
   console.log(`I am 1 strange loop.`);
   }
   else {
-    console.log(`I am ${i} strange loops.`)
+    console.log(`I am ${arr[i]} strange loops.`)
     }
   }
   return arr;
