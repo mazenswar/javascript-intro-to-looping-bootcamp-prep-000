9447ddaf3d9175846344fa5fed54e1d30a335ce3
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 22:10:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
 function forLoop(arr) {
-for (let i=0; i<=25; i++) {
-  if(i === 1) {
+for (let i=0; i<25; i++) {
+  if(arr[i] === 1) {
   console.log(`I am 1 strange loop`);
   }
   else {
     console.log(`I am ${i} strange loops.`)
     }
   }
+  return arr;
 }
